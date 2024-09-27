7609ceec2d2346ff8f8442615c569a35cce2d3e9
Xploit999 <jhon.piedrahita@outlook.com>
2024-09-26 21:07:31 -0500

    Se da formato al archivo de variables

--- digitalocean_vpc/variables.tf
+++ digitalocean_vpc/variables.tf
@@ -1,14 +1,14 @@
 variable "nombre_vpc" {
-  type = string
+  type    = string
   default = "mi_vpc"
 }
 
 variable "region" {
-  type = string
+  type    = string
   default = "nyc1"
 }
 
 variable "ip_rango" {
-  type = string
+  type    = string
   default = "10.0.0.0/24"
 }
